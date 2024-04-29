8800d05d4e442d3032a023479e6d3fa4f996d594
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-29 17:50:32 +0200

    Ajout de la class card

---
+++ Card.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Card
+    {
+
+        public Card(Value value, Type type)
+        {
+            this.Val = value;
+            this.Typ = type;
+        }
+
+        public Value Val 
+        { 
+            get; set; 
+        }
+        public Type Typ 
+        { 
+            get; set; 
+        }
+
+        public enum Value
+        {
+            As,
+            Deux,
+            Trois,
+            Quatre,
+            Cinq,
+            Six,
+            Sept,
+            Huit, 
+            Neuf,
+            Dix,
+            Valet,
+            Dame,
+            Roi,
+        }
+
+       public enum Type
+        {
+            Trefle,
+            Carreaux,
+            Pique,
+            Coeur,
+        }
+    }
+}
