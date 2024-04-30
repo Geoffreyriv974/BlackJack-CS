14d3ddc9619d6d3ea72993e2e9370685b76489aa
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-30 10:46:37 +0200

    Ajout de la class Game

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BlackJack.Card;
+
+namespace BlackJack
+{
+    public class Game
+    {
+
+        public void Run()
+        {
+            Sabot sabot = new Sabot(6);
+            sabot.Fill();
+        }
+
+    }
+}
+ 
