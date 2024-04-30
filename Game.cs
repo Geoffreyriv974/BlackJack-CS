c976a52e77a31895645c16abc762ad831f0fb7e8
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-30 12:04:47 +0200

    demande de lancement de la partie

--- Game.cs
+++ Game.cs
@@ -12,8 +12,37 @@ namespace BlackJack
 
         public void Run()
         {
-            Sabot sabot = new Sabot(6);
-            sabot.Fill();
+            Console.WriteLine("Bonjour, Bienvenus au BlackJack");
+
+            Console.Write("Quel est votre nom ? ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Bonjour, {userName}! Je m'appelle Geoffrey, je serai le Croupier lors de cette partie!");
+
+            string start = "";
+
+            while (start != "yes" && start != "no")
+            {
+                Console.Write("Voulez vous commencer une partie ? (yes/no)");
+                start = Console.ReadLine();
+
+                if (start == "yes")
+                {
+                    Console.WriteLine("1");
+                }
+                else if (start == "no")
+                {
+                    Console.WriteLine("A bientôt");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Vous ne répondez pas a la question !");
+                }
+            }
+            
+            //Sabot sabot = new Sabot(6);
+            //sabot.Fill();
         }
 
     }
