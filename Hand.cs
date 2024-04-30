34b8fe4ea2771207265c6da06d11cfb957b470b7
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-30 17:10:07 +0200

    Ajout de la valeur de la main de la main

--- Hand.cs
+++ Hand.cs
@@ -11,6 +11,43 @@ namespace BlackJack
 
         private List<Card> HandCard = new List<Card>();
 
+        public void sort() 
+        {
+            HandCard.Sort((x, y) =>
+            {
+                if (x.Val == Card.Value.As && y.Val != Card.Value.As)
+                    return 1;
+                else if (x.Val != Card.Value.As && y.Val == Card.Value.As)
+                    return -1;
+                else
+                    return 0;
+            });
+        }
+
+        public int Score
+        {
+            get
+            {
+
+                int addScore = 0;
+
+                foreach (Card item in HandCard)
+                {
+
+                    if (item.Val == Card.Value.As && addScore + 11 > 21)
+                    {
+                        addScore += 1;
+                    }
+                    else
+                    {
+                        addScore += (int)item.Val;
+                    }
+                }
+
+                return addScore;
+            }
+        }
+
         public void AddCard(Card card) 
         {
             HandCard.Add(card);
@@ -20,10 +57,12 @@ namespace BlackJack
         {
             Console.WriteLine("\n-----------------");
             Console.WriteLine("Cartes dans la main :");
+            sort();
             foreach (Card item in HandCard)
             {
                 Console.WriteLine("\n" + item.Val + " de " + item.Typ);
             }
+            Console.WriteLine("\nVotre Score : " + this.Score);
             Console.WriteLine("-----------------\n");
         }
 
