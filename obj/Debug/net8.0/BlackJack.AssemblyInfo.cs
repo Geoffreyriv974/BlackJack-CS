0521a4addfaa92242ecaac3d7543dbf0f20ae081
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-30 10:44:31 +0200

    fichier obj

--- obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e60563711036caf1d62529824e45c9744f12338")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8800d05d4e442d3032a023479e6d3fa4f996d594")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
