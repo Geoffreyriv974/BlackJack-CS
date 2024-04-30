8471cc8a13907451dce8187fd194de14f75cb696
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-30 16:30:31 +0200

    Ajout des fonctions pour ajouter et regarder la main

--- Hand.cs
+++ Hand.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-    internal class Hand
+    public class Hand
     {
+
+        private List<Card> HandCard = new List<Card>();
+
+        public void AddCard(Card card) 
+        {
+            HandCard.Add(card);
+        }
+
+        public void ViewCard()
+        {
+            Console.WriteLine("\n-----------------");
+            Console.WriteLine("Cartes dans la main :");
+            foreach (Card item in HandCard)
+            {
+                Console.WriteLine("\n" + item.Val + " de " + item.Typ);
+            }
+            Console.WriteLine("-----------------\n");
+        }
+
     }
 }
